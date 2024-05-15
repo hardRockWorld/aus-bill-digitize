a8083565a4797c0b9db19abca7104a5e0b35310f
Gourab Mahapatra <82425081+hardRockWorld@users.noreply.github.com>
2024-05-15 14:44:26 +0530

    add primeVue, implement setup & config for it (#1)
    downloaded the PrimeVue component presents zip file from GitHub repo and unzipped, pasted in our project dir.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from "vue";
 import "./style.css";
+import PrimeVue from "primevue/config";
+import Aura from "./presets/aura"; // import the Aura preset
 import App from "./App.vue";
 
 const app = createApp(App);
+
+// use the vue plugin for primeVue
+app.use(PrimeVue, {
+  unstyled: true,
+  pt: Aura, // use the Aura preset
+});
+
 app.mount("#app");
