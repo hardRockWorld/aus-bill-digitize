cae201a544ba552bc8008d41590dbe4492515dc5
Gourab Mahapatra <mgourab@hotmail.com>
2024-05-16 10:47:00 +0530

    code the router, view files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script setup>
+import { RouterLink, RouterView, useRoute, useRouter } from "vue-router";
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold text-black">
-    Welcome to AUS Bill Digitize
-  </h1>
+  <header>
+    <nav>
+      <ul>
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/orders">Order Entry</RouterLink></li>
+      </ul>
+      <h4>Welcome to Bill Digitize</h4>
+      <ul>
+        <li><RouterLink to="/login">Login</RouterLink></li>
+        <li v-if="proxyIsLoggedIn">
+          <a href="#" @click.prevent="logout" class="contrast">Logout</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main></main>
+  <footer>
+    <div>
+      <small>Made with 💛 by <big>Gourab</big></small>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
+nav {
+  border-bottom: solid 1px var(--secondary);
+  display: flex;
+  flex-direction: column;
+}
 </style>
